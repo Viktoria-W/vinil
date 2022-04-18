0c8237c9cba795e76b51ba887a63a7d2333cef21
Victoria <vicktorianedopekina@yandex.ru>
2022-04-18 19:33:53 +0400

    added array sound

--- src/js/app.js
+++ src/js/app.js
@@ -6,20 +6,39 @@ flsFunctions.isWebp();
 
 
 //Анимация пластинки и нот
+
 const recordBody = document.querySelector('.record__body');
 const note = document.querySelectorAll('.note__img');
 const noteList = document.querySelector('.note__container');
+let audioArray = [
+    '../files/sound/0-vinyl-record.mp3',
+    '../files/sound/1-hip-hop-n-jazz.mp3',
+    '../files/sound/3-session-joda-mstr.mp3',
+    '../files/sound/4-all-i-need.mp3',
+    '../files/sound/5-day-dreaming-andi-remix.mp3',
+    '../files/sound/6-gone-get-it-n89man-remix.mp3',
+    '../files/sound/7-wataboi-wake-up-ft-romy-dya.mp3',
+];
 
-
-//нажатие кнопок
+//нажатие кнопок при клике и при касании с телефона
 document.querySelector('.button-play').addEventListener("click", function () {
     play();
+    playSound();
 });
 document.querySelector(".button-stop").addEventListener("click", function () {
     stop();
+    stopSound();
+});
+document.querySelector('.button-play').addEventListener("touchend", function () {
+    play();
+    playSound();
+});
+document.querySelector(".button-stop").addEventListener("touchend", function () {
+    stop();
+    stopSound();
 });
 
-//функция старт
+//функция старт анимации
 function play () {
     if (recordBody.classList.contains('record-rotate')) {
         recordBody.classList.remove('record-rotate');
@@ -31,7 +50,25 @@ function play () {
     }
 }
 
-//функция стоп
+//Воспроизведение песен по очереди из массива
+function playSound() {
+    let player = document.getElementById('player');
+    let current = 0;
+    player.src = audioArray[0];
+    player.onended = function() {
+        current++;
+        if (current >= audioArray.length) current = 0;
+        player.src = audioArray[current];
+        player.play();
+    };
+}
+
+function stopSound() {
+    let player = document.getElementById('player');
+    player.pause();
+}
+
+//функция стоп анимации
 function stop () {
     if (recordBody.classList.contains('record-play')) {
         recordBody.classList.remove('record-play');
