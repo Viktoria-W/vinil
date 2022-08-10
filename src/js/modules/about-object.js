39163cbef4cdc40402f751567550dec2be62b84e
Victoria <vicktorianedopekina@yandex.ru>
2022-08-10 16:49:43 +0400

    change about style

--- src/js/modules/about-object.js
+++ src/js/modules/about-object.js
@@ -1,4 +1,13 @@
 export const aboutVinyl = {
-    title: `Винил с любого коспьютера`,
-    text: `Теперь что бы аослушать винил.....`
+    titleAbout: `About`,
+    titleVinyl: `Vinyl from any computer`,
+    textVinyl: `Now, in order to listen to vinyl records, it is not necessary to buy a player. You can enjoy vinyl directly in your browser.`,
+    titleOptions: `Expanding your options`,
+    textOptions: `In the early versions of Wayne Vinil, only one record was available, now there are three available, and more will be added. Easy navigation and track highlighting will help you navigate the playlist.`,
+    titleStyle: `Choose your style`,
+    textStyle: `When you try to rewind a record on a vinyl player in reality, there is a possibility of damaging it, for this reason, this logic has been transferred here. Great opportunity to listen to the whole album.`,
+    titleResult: `Enjoy the result`,
+    textResult: `You have three of the most popular styles of music on the planet at your disposal. Each disc contains a collection of the selected direction. On the rock record, you can find tracks in the style of pop rock, indie rock and rock 'n' roll. In the pop list you will find synth-pop, dance-pop and traditional pop music. Vinyl hip-hop record has got new tracks combining jazz rap, lo-fi hip hop and old-skool. See for yourself. Choose your style and enjoy the music.`,
+    titlePrototype: `A new look at the familiar`,
+    textPrototype: `The author's home vinyl player served as a prototype for creating the design. By retaining all the logic and using the capabilities of the browser, the author hopes that he managed to push everyone to the magic of vinyl.`,
 }
--- src/js/modules/about.js
+++ src/js/modules/about.js
@@ -6,7 +6,7 @@ export default class About {
     constructor(object) {
         this.object = object;
         this.template = this.#template();
-        this.#elem =  this.render();
+        this.#elem = this.render();
     }
 
     render() {
@@ -18,7 +18,58 @@ export default class About {
     #template() {
         return createElement(
             `
-            <h1 class = "about__title">${this.object.title}</h1>s
+            <div class="about__wrapper">
+                <div class = "about__title">${this.object.titleAbout}</div>
+                <div class ="about__button-block">
+                    <button type ="button" class ="about__button" id ="en">En</button>
+                    <button type ="button" class ="about__button" id ="ru">Ru</button>
+                </div>
+                <div class="about__vinyl vinyl">
+                    <div class ="vinyl__container container">
+                        <h3 class="vinyl__title title">${this.object.titleVinyl}</h3>
+                        <div class="vinyl__text-container">
+                            <p class="vinyl__text text">${this.object.textVinyl}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="about__options options">
+                    <div class ="options__img-block"></div>
+                    <div class ="options__container container">
+                        <h3 class="options__title title">${this.object.titleOptions}</h3>
+                        <div class="options__text-container">
+                            <p class="options__text text">${this.object.textOptions}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="about__style style">
+                    <div class ="style__img-block"></div>
+                    <div class ="style__container container">
+                        <h3 class="style__title title">${this.object.titleStyle}</h3>
+                            <div class ="style__text-wrapper">
+                                <p class="style__text text">${this.object.textStyle}</p>
+                            </div>
+                    </div>
+                </div>
+                <div class="about__result result">
+                    <div class ="result__container container">
+                        <h3 class="result__title title">${this.object.titleResult}</h3>
+                        <div class="result__text-container">
+                            <div class ="result__text-wrapper">
+                                <p class="result__text text">${this.object.textResult}</p>
+                            </div>
+                            <img src="../../img/vinyl-result.jpg" alt="Vinyl record" class = "result__img">
+                        </div>
+                    </div>
+                </div>
+                <div class="about__prototype prototype">
+                    <div class ="prototype__container container">
+                        <h3 class="prototype__title title">${this.object.titlePrototype}</h3>
+                        <div class="prototype__text-container">
+                            <p class="prototype__text text">${this.object.textPrototype}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             `
         );
     }
